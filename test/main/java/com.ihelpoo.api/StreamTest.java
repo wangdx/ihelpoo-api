6e8b4300a0671a333652fe0740b2d9e3ad329151
wangdx <echowdx@gmail.com>
2013-06-02 23:48:27 +0800

    add unit test framework

---
+++ test/main/java/com.ihelpoo.api/StreamTest.java
@@ -0,0 +1,12 @@
+package com.ihelpoo.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class StreamTest {
+    @Test
+    public void something(){
+        fail();
+    }
+}
